84e40bf28d57b2fb124404947ac804e234e17e9d
Matias <matigfv@gmail.com>
2022-03-26 20:49:34 -0300

    #pom

--- src/environments/enviroment.default.ts
+++ src/environments/enviroment.default.ts
@@ -2,7 +2,7 @@ import { Environment } from './enviroment.model';
 
 export const environment: Environment = {
   api: '/',
-  baseUrl: 'http://localhost:8080',
-  //baseUrl: 'https://larrata-api.herokuapp.com',
+  //baseUrl: 'http://localhost:8080',
+  baseUrl: 'https://larrata-api.herokuapp.com',
   production: false
 };
